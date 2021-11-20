22a691d7a13afa889f870927c73cf8c7a53385bc
FrancescoLenolli <lenolli.f@gmail.com>
2021-11-20 22:11:30 +0100

    Working on DialogueSystem
    Simple dialogue system, base dialogue data and system done, still unsure on proper nomenclature

---
+++ Package-UIFramework/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+public class DialogueSystem : Singleton<DialogueSystem>
+{
+    public DialogueTree testDialogueTree;
+
+    [SerializeField]
+    private GameObject dialogueCanvas = null;
+    [SerializeField]
+    private TextMeshProUGUI dialogueText = null;
+    [SerializeField]
+    private float dialogueDelay = 1f;
+
+    private DialogueTree currentDialogueTree;
+    private bool canAutoplay = false;
+
+    private void Start()
+    {
+        SetDialogueTree(testDialogueTree);
+    }
+
+    public void SetDialogueTree(DialogueTree newDialogueTree)
+    {
+        currentDialogueTree = newDialogueTree;
+        StartDialogue();
+    }
+
+    public void AutoPlayDialogues()
+    {
+        canAutoplay = !canAutoplay;
+    }
+
+    private void StartDialogue()
+    {
+        dialogueCanvas.SetActive(true);
+        StartCoroutine(ShowDialogueRoutine());
+    }
+
+    private void EndDialogue()
+    {
+        dialogueCanvas.SetActive(false);
+    }
+
+    private IEnumerator ShowDialogueRoutine()
+    {
+        int index = 0;
+        int lastIndex = currentDialogueTree.dialogues.Count;
+        bool canEndDialogue = false;
+
+        while (!canEndDialogue)
+        {
+            bool areDialoguesEnded = index >= lastIndex;
+
+            if (!areDialoguesEnded)
+            {
+                dialogueText.text = dialogueText.text.Remove(0);
+                foreach (char character in currentDialogueTree.dialogues[index].text)
+                {
+                    dialogueText.text += character;
+                    yield return new WaitForSeconds(0.05f);
+                }
+                ++index;
+            }
+
+            if (!canAutoplay)
+            {
+                while (!Input.GetKeyDown(KeyCode.Space) && !canAutoplay)
+                { yield return null; }
+            }
+            else if(!areDialoguesEnded)
+            {
+                yield return new WaitForSeconds(dialogueDelay);
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space) && areDialoguesEnded)
+            {
+                canEndDialogue = true;
+                EndDialogue();
+            }
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+}
---
+++ Package-UIFramework/Assets/Scripts/DialogueTree.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/DialogueSystem/DialogueTree", fileName = "NewDialogueTree")]
+public class DialogueTree : ScriptableObject
+{
+    public enum CharacterStatus { Normal, Angry }
+
+    [System.Serializable]
+    public struct Dialogue
+    {
+        public string characterName;
+        public CharacterStatus status;
+        [TextArea]
+        public string text;
+    }
+
+    public List<Dialogue> dialogues = new List<Dialogue>();
+}
