4c9ce95471d84995f46cb46bcea4a8e5ae5c8bca
FrancescoLenolli <lenolli.f@gmail.com>
2022-03-21 22:59:22 +0100

    Updated Service Locator pattern
    Tried some things, settled on making the Service Locator a static class.

--- Package-UIFramework/Assets/Scripts/ServiceLocator.cs
+++ Package-UIFramework/Assets/Scripts/ServiceLocator.cs
@@ -3,36 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
- * Implementation of the Service Locator design pattern.
- * It's not a static or singleton class because it's designed to be utilised by another class
- * to get only the references it needs.
- * Example: Player class only needs a reference to GameManager and UIState only needs access to a UIManager,
- * and they each use a local ServiceLocator.
- * Don't know if it's a sound design decision yet, I need more research.
- */
-public class ServiceLocator
+// Implementation of the Service Locator design pattern.
+public static class ServiceLocator
 {
-    private Dictionary<Type, MonoBehaviour> serviceReferences;
+    private static Dictionary<Type, MonoBehaviour> services = new Dictionary<Type, MonoBehaviour>();
 
-    public ServiceLocator()
+    public static T GetService<T>() where T : MonoBehaviour, new()
     {
-        serviceReferences = new Dictionary<Type, MonoBehaviour>();
-    }
-
-    public T GetService<T>() where T: MonoBehaviour, new()
-    {
-        UnityEngine.Assertions.Assert.IsNotNull(serviceReferences,
+        UnityEngine.Assertions.Assert.IsNotNull(services,
             "someone has requested a service prior to the locator's initialisation.");
 
-        bool serviceLocated = serviceReferences.ContainsKey(typeof(T));
+        bool serviceLocated = services.ContainsKey(typeof(T));
         if (!serviceLocated)
-            serviceReferences.Add(typeof(T), GameObject.FindObjectOfType<T>());
+            services.Add(typeof(T), GameObject.FindObjectOfType<T>());
 
-        UnityEngine.Assertions.Assert.IsTrue(serviceReferences.ContainsKey(typeof(T)), $"Could not find service {typeof(T)}.");
-        var service = (T)serviceReferences[typeof(T)];
+        UnityEngine.Assertions.Assert.IsTrue(services.ContainsKey(typeof(T)), $"Could not find service {typeof(T)}.");
+        var service = (T)services[typeof(T)];
         UnityEngine.Assertions.Assert.IsNotNull(service,
-            $"Service {typeof(T)} in Scene {UnityEngine.SceneManagement.SceneManager.GetActiveScene().name} could not be found.");
+            $"Service '{typeof(T)}' in Scene '{UnityEngine.SceneManagement.SceneManager.GetActiveScene().name}' could not be found.");
 
         return service;
     }
